0b0b1fa2403f9c71e8423831e04957af271f5e40
azmain-siam <siamhossain2700@gmail.com>
2026-04-26 04:29:35 +0600

    feat: current user fetch route

--- src/module/user/user.controller.ts
+++ src/module/user/user.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import { GetUser } from 'src/common/decorators/get-user.decorator';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @ApiOperation({ summary: 'Get current user' })
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getMe(@GetUser('sub') userId: string) {
+    return this.userService.getMe(userId);
+  }
 }
--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { ApiResponse } from 'src/common/response/api-response';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async getMe(userId: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+      include: {
+        client: true,
+      },
+    });
+
+    return ApiResponse.success('User fetched successfully', user);
+  }
 }
