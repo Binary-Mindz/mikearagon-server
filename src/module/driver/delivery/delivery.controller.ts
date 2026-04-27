b7d6735788b07171f2d68aa2a613518a451c0112
azmain-siam <siamhossain2700@gmail.com>
2026-04-28 04:25:50 +0600

    fix: some issues

--- src/module/driver/delivery/delivery.controller.ts
+++ src/module/driver/delivery/delivery.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Role } from '@prisma/client';
 import { GetUser } from 'src/common/decorators/get-user.decorator';
 import { Roles } from 'src/common/decorators/roles.decorator';
@@ -13,9 +13,18 @@ export class DeliveryController {
   constructor(private readonly deliveryService: DeliveryService) {}
 
   @Get('my-route')
+  @ApiOperation({ summary: 'Get my route orders' })
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.DRIVER)
   getMyRouteOrders(@GetUser('sub') userId: string) {
     return this.deliveryService.getMyRouteOrders(userId);
   }
+
+  // @Get('my-route/:clientId')
+  // @ApiOperation({ summary: 'Get my route orders by client' })
+  // @UseGuards(JwtAuthGuard, RolesGuard)
+  // @Roles(Role.DRIVER)
+  // getMyRouteOrdersByClient(@GetUser('sub') userId: string) {
+  //   return this.deliveryService.getMyRouteOrdersByClient(userId);
+  // }
 }
